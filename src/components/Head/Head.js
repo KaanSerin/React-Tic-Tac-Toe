cc37147fac4a739cf96ec1c05db7343968b3fff7
Kaan Serin <kaanserin113@gmail.com>
2020-06-21 22:14:43 +0300

    board evaluation works! (brute force)

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -6,7 +6,9 @@ const head = (props) => (
   <Aux>
     <h1 style={{ color: "white" }}>TIC-TAC-TOE</h1>
     <MenuButtons resetBoard={props.clearBoard} />
-    <h2 style={{ display: props.gameOver ? "block" : "none" }}>Game Over!</h2>
+    <h2 style={{ color: "white", display: props.gameOver ? "block" : "none" }}>
+      Game Over! {props.playerWon} is Victorious!
+    </h2>
     <p style={{ color: "white", fontSize: "2rem", margin: "10px" }}>
       Turn: {props.turn}
     </p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -17,16 +17,30 @@ class App extends Component {
       box7: null,
       box8: null,
     },
+    magicBoard: {
+      box0: 8,
+      box1: 1,
+      box2: 6,
+      box3: 3,
+      box4: 5,
+      box5: 7,
+      box6: 4,
+      box7: 9,
+      box8: 2,
+    },
+    p1LastThreeMoves: [],
+    p2LastThreeMoves: [],
     currentPlayer: "X",
+    playerWon: null,
     gameOver: false,
   };
 
   boxClickHandler = (box) => {
-    const newBoard = this.state.board;
-    newBoard[box] = this.state.currentPlayer;
-    const newCurrentPlayer = this.state.currentPlayer === "X" ? "O" : "X";
-    this.setState({ board: newBoard, currentPlayer: newCurrentPlayer });
-    this.evaluateBoard(this.state.board);
+    if (!this.state.gameOver && this.state.board[box] === null) {
+      const newBoard = this.state.board;
+      newBoard[box] = this.state.currentPlayer;
+      this.evaluateBoard(this.state.currentPlayer, newBoard);
+    }
   };
 
   clearBoard = () => {
@@ -45,7 +59,39 @@ class App extends Component {
       currentPlayer: "X",
     });
   };
-  evaluateBoard = (board) => {};
+
+  evaluateBoard = (currentPlayer, newBoard) => {
+    const newCurrentPlayer = this.state.currentPlayer === "X" ? "O" : "X";
+
+    const winningCombos = [
+      ["box0", "box1", "box2"],
+      ["box3", "box4", "box5"],
+      ["box6", "box7", "box8"],
+      ["box0", "box3", "box6"],
+      ["box1", "box4", "box7"],
+      ["box2", "box5", "box8"],
+      ["box0", "box4", "box8"],
+      ["box2", "box4", "box6"],
+    ];
+
+    winningCombos.forEach((combo) => {
+      // If there is a winning combo, set the game to be over and declare the winner
+      if (
+        (newBoard[combo[0]] !== null) &
+          (newBoard[combo[0]] === newBoard[combo[1]]) &&
+        newBoard[combo[0]] === newBoard[combo[2]]
+      ) {
+        const newPlayerWon = newBoard[combo[0]];
+        this.setState({ gameOver: true, playerWon: newPlayerWon });
+      }
+    });
+
+    // In any case, set the new board and player state
+    this.setState({
+      currentPlayer: newCurrentPlayer,
+      board: newBoard,
+    });
+  };
 
   render() {
     return (
@@ -54,6 +100,7 @@ class App extends Component {
           gameOver={this.state.gameOver}
           turn={this.state.currentPlayer}
           clearBoard={this.clearBoard}
+          playerWon={this.state.playerWon}
         />
         <Board board={this.state.board} clickHandler={this.boxClickHandler} />
       </div>
